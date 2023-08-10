ba11f5c4f1a88f48e31e891502cd5d5f248d9a66
Boworn <Boworn.tr@gmail.com>
2023-08-10 13:59:55 +0700

    Revert "Complete add tailwind"
    This reverts commit e2b52941c356d8f4dc2684b7cfb57df8f397941f.

--- client/src/app/site/component/navbar.jsx
+++ client/src/app/site/component/navbar.jsx
@@ -3,10 +3,7 @@ import React from "react";
 function Navbar() {
   return (
     <>
-      <div className=" text-red-500">navbar</div>
-      <div>
-        <div className="text-8xl">Test</div>
-      </div>
+      <div>navbar</div>;
     </>
   );
 }
